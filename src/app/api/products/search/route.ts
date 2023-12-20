caecf0ea5e98261bedd835cf075bfe4b543f3af3
Joao Carlos <joaocpinheiro03@gmail.com>
2023-12-20 18:53:20 -0300

    add search page, current-search, fix search router

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -1,18 +1,17 @@
 import { NextRequest } from 'next/server'
-import data from '../data.json'
+import data from '../../data.json'
 import { z } from 'zod'
 
 export async function GET(request: NextRequest) {
   await new Promise((resolve) => setTimeout(resolve, 1000))
 
-  const { searchParams } = await request.nextUrl
+  const { searchParams } = request.nextUrl
 
   const query = z.string().parse(searchParams.get('q'))
-  console.log(query)
 
-  const product = data.products.filter((product) => {
+  const products = data.products.filter((product) => {
     return product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
   })
 
-  return Response.json(product)
+  return Response.json(products)
 }
