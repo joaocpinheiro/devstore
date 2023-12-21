04a6ccb78d22cb98ca6cf841f82a835a6f22a41a
Joao Carlos <joaocpinheiro03@gmail.com>
2023-12-21 19:30:08 -0300

    chore: add client/server env variables boundary

--- src/env.ts
+++ src/env.ts
@@ -5,10 +5,6 @@ import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
 export const env = createEnv({
-  /*
-   * Serverside Environment variables, not available on the client.
-   * Will throw if you access these variables on the client.
-   */
   server: {
     APP_URL: z.string().url(),
   },
